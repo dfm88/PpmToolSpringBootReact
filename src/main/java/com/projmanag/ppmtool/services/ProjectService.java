d7eb69a7aa99fc006ad92e279cb289c4d805c460
dfm88 <diegof.university@gmail.com>
2021-03-20 22:05:59 +0100

    Gestito l'errore lanciato dal BD quando viene inserito un progetto con Identifier (unique) uguale

--- src/main/java/com/projmanag/ppmtool/services/ProjectService.java
+++ src/main/java/com/projmanag/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projmanag.ppmtool.domain.Project;
+import com.projmanag.ppmtool.exceptions.ProjectIdException;
 import com.projmanag.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -14,9 +15,14 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project proj) {
 		
-		//Logic
+		try {
+			proj.setProjectIdentifier(proj.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(proj);		
+			
+		} catch (Exception e) {
+			throw new ProjectIdException("Poject ID '"+proj.getProjectIdentifier().toUpperCase()+"' already exists");
+		}
 		
-		return projectRepository.save(proj);
 	}
 
 }
