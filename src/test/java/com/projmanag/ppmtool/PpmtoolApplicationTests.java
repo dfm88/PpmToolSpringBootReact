c9103dc154ea7afc59ab95e581292685b5e55c37
dfm88 <diegof.university@gmail.com>
2021-03-20 02:14:36 +0100

    first commit

---
+++ src/test/java/com/projmanag/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projmanag.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
