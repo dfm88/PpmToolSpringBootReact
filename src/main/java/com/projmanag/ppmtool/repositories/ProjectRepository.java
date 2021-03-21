9fb61b3fbea9cf076bf5b32c58968443f48af5ae
dfm88 <diegof.university@gmail.com>
2021-03-21 01:57:44 +0100

    implemntata GetAll e GeById

--- src/main/java/com/projmanag/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/com/projmanag/ppmtool/repositories/ProjectRepository.java
@@ -8,11 +8,12 @@ import com.projmanag.ppmtool.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
+	Project findByProjectIdentifier(String projectIdentifier);
+
 	@Override
-	default Iterable<Project> findAllById(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	Iterable<Project> findAll();
+	
+	
 	
 	
 	
--- src/main/java/com/projmanag/ppmtool/services/ProjectService.java
+++ src/main/java/com/projmanag/ppmtool/services/ProjectService.java
@@ -13,16 +13,32 @@ public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 	
-	public Project saveOrUpdateProject(Project proj) {
-		
+	public Project saveOrUpdateProject(Project proj) {		
 		try {
 			proj.setProjectIdentifier(proj.getProjectIdentifier().toUpperCase());
 			return projectRepository.save(proj);		
 			
 		} catch (Exception e) {
 			throw new ProjectIdException("Poject ID '"+proj.getProjectIdentifier().toUpperCase()+"' already exists");
+		}		
+	}
+	
+	public Project findProjectByIdentifier (String projectID) {
+		
+		Project project = projectRepository.findByProjectIdentifier(projectID.toUpperCase());
+		
+		if(project ==null) {
+			throw new ProjectIdException("Poject ID '"+projectID+"' doesn't exists");
 		}
 		
+		return project;
 	}
+	
+	public Iterable<Project> findAllProject() {
+	
+		return projectRepository.findAll();
+		
+	}
+	
 
 }
